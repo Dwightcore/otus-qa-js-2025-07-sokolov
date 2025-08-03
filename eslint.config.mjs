f22a3626a7ef81df4b9fcbd6dcf0db0a00814e03
m.sokolov <m.sokolov@xsolla.com>
2025-08-03 10:59:55 +0500

    feat [Homework-5] Add unit tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,17 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslintPluginJest from 'eslint-plugin-jest'
 
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
-  eslintPluginPrettierRecommended
+  eslintPluginPrettierRecommended,
+  {
+    plugins: { jest: eslintPluginJest },
+    languageOptions: { globals: { ...globals.jest } },
+    rules: {
+      ...eslintPluginJest.configs.recommended.rules
+    }
+  }
 ]
