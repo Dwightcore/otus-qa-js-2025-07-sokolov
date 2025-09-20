9215b7182643f78e5ec325e152965b17fa4ddeec
m.sokolov <m.sokolov@xsolla.com>
2025-09-20 17:44:30 +0500

    feat [Homework-11] add reporter

--- framework/services/authService.js
+++ framework/services/authService.js
@@ -3,7 +3,6 @@ import config from '../config/config.js'
 
 export const authService = {
   async authorisation(userName, password) {
-    console.log('Auth request data:', { userName, password })
     const response = await got.post(`${config.api.baseUrl}/Account/v1/Authorized`, {
       json: {
         userName,
