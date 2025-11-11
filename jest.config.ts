f1d43ba11d7805a6ac3dcff074b1d749774b401d
m.sokolov <m.sokolov@xsolla.com>
2025-11-11 19:26:44 +0500

    add docker and small fixes

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,27 @@
-export default {
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'allure-jest/node',
-  testEnvironmentOptions: {
-    resultsDir: 'reports/allure-results'
-  },
+  testEnvironmentOptions: { resultsDir: 'reports/allure-results' },
+
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
-  }
-}
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testMatch: ['<rootDir>/(specs|tests)/**/*.test.ts']
+};
+
+export default config;
