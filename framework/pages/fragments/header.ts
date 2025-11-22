51a140cc6e1288e465bf28040e508ce67477a0b4
m.sokolov <m.sokolov@xsolla.com>
2025-11-22 20:46:18 +0500

    homework-13

---
+++ framework/pages/fragments/header.ts
@@ -0,0 +1,29 @@
+import { Locator, Page } from '@playwright/test';
+
+export default class Header {
+  private page: Page;
+  private root: Locator;
+  private loginLink: Locator;
+  private logoutLink: Locator;
+  private registerLink: Locator;
+  private wishlistLink: Locator;
+  private cartLink: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.root = page.locator('.header');
+    this.loginLink = this.root.getByRole('link', { name: 'Log in' });
+    this.logoutLink = this.root.getByRole('link', { name: 'Log out' });
+    this.registerLink = this.root.getByRole('link', { name: 'Register' });
+    this.wishlistLink = this.root.getByRole('link', { name: /Wishlist/ });
+    this.cartLink = this.root.getByRole('link', { name: /Shopping cart/ });
+  }
+
+  async openLogin(): Promise<void> { await this.loginLink.click(); }
+  async openRegister(): Promise<void> { await this.registerLink.click(); }
+  async openWishlist(): Promise<void> { await this.wishlistLink.click(); }
+  async openCart(): Promise<void> { await this.cartLink.click(); }
+  async logoutIfVisible(): Promise<void> {
+    if (await this.logoutLink.isVisible()) await this.logoutLink.click();
+  }
+}
---
+++ framework/pages/product-page.ts
@@ -0,0 +1,22 @@
+import { Locator, Page } from '@playwright/test';
+import BasePage from './base-page.js';
+
+export default class ProductPage extends BasePage {
+  private form: Locator;
+  private addToCart: Locator;
+  
+
+  constructor(page: Page) {
+    super(page);
+    this.form = page.locator('#product-details-form');
+    this.addToCart = page.locator('#add-to-cart-button-2');
+  }
+
+  async openBySlug(slug: string): Promise<void> {
+    await super.open(`/${slug}`);
+  }
+
+  async addToCartItem(): Promise<void> {
+    await this.addToCart.click();
+  }
+}
